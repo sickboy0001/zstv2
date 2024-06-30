bd8694484784bb7a5efa988ebf5491906f4692c8
tfujiki <syunjyu0001@gmail.com>
2024-06-30 17:16:14 +0900

    ⚰️ 不要コードコメントアウト

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,24 +1,24 @@
-import DeployButton from "@/components/DeployButton";
-import AuthButton from "@/components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
-import Header from "@/components/Header";
+// import DeployButton from "@/components/DeployButton";
+// import AuthButton from "@/components/AuthButton";
+// import { createClient } from "@/utils/supabase/server";
+// import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
+// import Header from "@/components/Header";
 import { redirect } from "next/navigation";
 
 export default async function ProtectedPage() {
-  const supabase = createClient();
+  // const supabase = createClient();
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  // const {
+  //   data: { user },
+  // } = await supabase.auth.getUser();
 
-  if (!user) {
-    return redirect("/login");
-  }
+  // if (!user) {
+  //   return redirect("/login");
+  // }
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <div className="w-full">
+      {/* <div className="w-full">
         <div className="py-6 font-bold bg-purple-950 text-center">
           This is a protected page that you can only see as an authenticated
           user
@@ -37,9 +37,9 @@ export default async function ProtectedPage() {
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
           <FetchDataSteps />
         </main>
-      </div>
+      </div> */}
 
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+      {/* <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
           Powered by{" "}
           <a
@@ -51,7 +51,7 @@ export default async function ProtectedPage() {
             Supabase
           </a>
         </p>
-      </footer>
+      </footer> */}
     </div>
   );
 }
